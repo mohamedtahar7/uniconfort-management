c70eeae9f6370c7fb23fb72141f3417103674d2c
Mohamed Tahar Atallah <120719194+mohamedtahar7@users.noreply.github.com>
2024-07-01 21:17:51 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="py-5 flex items-center justify-center">
-      <h1 className="text-slate-800 text-3xl text-center font-semibold">
+      <h1 className="text-slate-800 text-lg sm:text-3xl text-center font-semibold">
         Welcome To Uniconfort&#39;s management Dashboard
       </h1>
     </main>
